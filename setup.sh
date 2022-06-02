4317235e190fad07f82e18427e0a536f5737bffd
jonathanl-telenav <jonathanl@telenav.com>
2022-06-02 17:09:50 -0600

    💚 Disable git flow configuration for ci build

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 # Determine the branch to set up
 branch_name=$1
+build=$2
 
 # shellcheck disable=SC2039
 if [[ -z "$branch_name" ]]; then
 
-    branch_name=develop
+    read -p "Branch [develop] ? " -r
+    echo " "
 
+    branch_name=$REPLY
+
+    if [[ -z "$branch_name" ]]; then
+
+        branch_name=develop
+
+    fi
 fi
 
 echo "Setting up to build branch $branch_name"
@@ -16,14 +25,18 @@ git submodule init || exit 1
 echo "Cloning git repositories"
 git submodule update || exit 1
 
-echo "Configuring git repositories"
-git config pull.ff only || exit 1
-# shellcheck disable=SC2016
-git submodule foreach 'git config pull.ff only && echo "Configuring $name"' || exit 1
+if [[ ! "$build" == "ci-build" ]]; then
 
-echo "Initializing git flow"
-# shellcheck disable=SC2016
-git submodule foreach '[[ "$path" == *-assets* ]] || git flow init -f -d --feature feature/ --bugfix bugfix/ --release release/ --hotfix hotfix/ --support support/ -t ''' || exit 1
+    echo "Configuring git repositories"
+    git config pull.ff only || exit 1
+    # shellcheck disable=SC2016
+    git submodule foreach 'git config pull.ff only && echo "Configuring $name"' || exit 1
+
+    echo "Initializing git flow"
+    # shellcheck disable=SC2016
+    git submodule foreach '[[ "$path" == *-assets* ]] || git flow init -f -d --feature feature/ --bugfix bugfix/ --release release/ --hotfix hotfix/ --support support/ -t ''' || exit 1
+
+fi
 
 echo "Configuring environment"
 # shellcheck disable=SC2039
