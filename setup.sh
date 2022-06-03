e8aaf688d54c563662f826221a7eab18bf790690
jonathanl-telenav <jonathanl@telenav.com>
2022-06-02 19:47:42 -0600

    💚 Set up superpom in batch mode

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Determine the branch to set up
 branch_name=$1
-build=$2
+caller=$2
+
+#
+# Determine branch to set up
+#
 
 # shellcheck disable=SC2039
 if [[ -z "$branch_name" ]]; then
@@ -19,14 +22,37 @@ if [[ -z "$branch_name" ]]; then
     fi
 fi
 
-echo "Setting up to build branch $branch_name"
+#
+# Initialize submodules
+#
+
+echo "Initializing submodules"
 git submodule init || exit 1
 
+#
+# Clone repositories
+#
+
 echo "Cloning git repositories"
-git submodule update || exit 1
+if [[ "$caller" == "ci-build" ]]; then
 
-if [[ "$build" == "ci-build" ]]; then
+    git submodule update --depth 1 || exit 1
 
+else
+
+    git submodule update || exit 1
+
+fi
+
+#
+# Configure repositories
+#
+
+echo "Configuring build"
+
+if [[ "$caller" == "ci-build" ]]; then
+
+    echo "Creating temporary folder"
     export TMPDIR=./temporary/
     mkdir $TMPDIR
 
@@ -34,6 +60,7 @@ else
 
     echo "Configuring git repositories"
     git config pull.ff only || exit 1
+
     # shellcheck disable=SC2016
     git submodule foreach 'git config pull.ff only && echo "Configuring $name"' || exit 1
 
@@ -43,9 +70,17 @@ else
 
 fi
 
+#
+# Configure environment
+#
+
 echo "Configuring environment"
 # shellcheck disable=SC2039
 source ./source-me || exit 1
+
+#
+# Check out branch
+#
 
 echo "Switching to branch $branch_name"
 export branch_name
@@ -54,8 +89,16 @@ git submodule --quiet foreach 'echo $path' | grep assets | xargs -I FOLDER echo 
 # shellcheck disable=SC2016
 git submodule --quiet foreach 'echo $path' | grep -v assets | xargs -I FOLDER echo "cd FOLDER && git checkout $branch_name"
 
+#
+# Build
+#
+
 echo "Building"
 mvn -f --batch-mode telenav-superpom/pom.xml clean install
 mvn --batch-mode clean install
+
+#
+# Setup complete
+#
 
 echo "Done."
