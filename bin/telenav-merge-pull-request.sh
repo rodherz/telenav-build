be1559fce4bb0ef58afcd45a33058394f0691520
jonathanl-telenav <jonathanl@telenav.com>
2022-08-08 15:31:41 -0600

    🔨 Add pull request merging

--- bin/telenav-merge-pull-request.sh
+++ bin/telenav-merge-pull-request.sh
@@ -16,27 +16,20 @@ if [[ -z "$TELENAV_GIT_AUTHENTICATION_TOKEN" ]]; then
 
 fi
 
-if [[ "$#" -eq 2 ]]; then
+if [[ "$#" -eq 0 ]]; then
 
-    title=$1
-    body=$2
+    branch_name=$(branch_name "$KIVAKIT_HOME")
 
-elif [[ "$#" -eq 0 ]]; then
+elif [[ "$#" -eq 1 ]]; then
 
-    read -p -r "Title? "
-    title=$REPLY
-    read -p -r "Body? "
-    body=$REPLY
+    branch_name=$1
 
 else
 
-    usage "([title] [body])?"
+    usage "[branch_name]?"
 
 fi
 
-
-cactus_all git-pull-request \
+cactus_all git-merge-pull-request \
     -Dcactus.authentication-token="$TELENAV_GIT_AUTHENTICATION_TOKEN" \
-    -Dcactus.title="$title" \
-    -Dcactus.body="$body" \
-    -Dcactus.reviewers="rodherz,sunshine-syz,timboudreau,wenjuanj,jonathanl-telenav,haifeng-z" || exit 1
+    -Dcactus.branch-name="$branch_name" || exit 1
